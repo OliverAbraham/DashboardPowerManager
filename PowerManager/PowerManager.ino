30677f9165d3ae1002b61b6402f1a4e9a4cd8892
Oliver Abraham <mail@oliver-abraham.de>
2024-12-15 18:52:51 +0100

    Added inertia and a wait time of 120 seconds after mouse click.

--- PowerManager/PowerManager.ino
+++ PowerManager/PowerManager.ino
@@ -1,34 +1,44 @@
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // # Dashboard power manager
 // 
 // ## OVERVIEW
-// Controls a tablet or raspi based dashboard for home automation or monitoring.Add a motion detector to your computer to save energy, switch it on only when somebody's around.
+// 
+// Controls a tablet or raspi based dashboard for home automation or monitoring.
+// Add a motion detector to your computer to save energy, switch it on only when somebody's around.
 // 
 // ## License
+// 
 // Licensed under Apache licence.
 // https://www.apache.org/licenses/LICENSE-2.0
 // 
 // 
 // ## Compatibility
+// 
 // This is source code for ESP8266 based microcontrollers, e.g.Wemos D1 mini or similiar.
 // 
 // 
 // ## DESCRIPTION
+// 
 // This program for a Wemos D1 mini or similiar ESP8266 controls an info display.
 // The purpose is to have a motion sensor attached and wake up the computer or screen saver when motion is detected.
 // 
 // ### Setup
+// 
 // I have a tablet computer(I call it display) with "normal" power saving settings :
 // Screensaver after some minutes, and go to hibernation after a longer time, i.e.one hour.
-// I tore out the board out of a cheap computer mouseand attached it to the display.
+// I tore out the board out of a cheap computer mouse and attached it to the display.
 // Pressing the left mouse button reactivates the display, as long as the display is under power.
-// I added a relay to my Wemos D1 that connects to the left mouse button.
-// This is a simpleand cheap method to wake up any computer with a microcontrollerand doesn't require USB logic.
-// Of couse this doesn't work for every display in power off state, because you cannot wake every system by a mouse click.
-// For this type I've atached a second line to the power button of the display. In my case this line only needs to be pulled low to switch it on.
-// If needed you can put a relay in between.
+// I added a relay to my Wemos D1 that connects its outputs to the left mouse button.
+// So switching the relay to on "presses" the mouse button.
+// This is a simple and cheap method to wake up any computer with a microcontroller and doesn't require USB logic.
+//
+// This might not work for every display in power off state, because you cannot wake every system by a mouse click.
+// To solve this, type I've attached a second line to the power button of the display. 
+// In my case this line only needs to be pulled low to switch it on. If needed, you can put a relay in between.
 // The program decides which method to use. "press" the power button or the mouse button.
 // 
 // ### Connections
+// 
 // So, the controller has two inputsand two outputs :
 // -Input 1 : signals motion, connect a standard 5 volt IR motion detector to it
 // -Input 2 : signals the power state of the display.
@@ -37,22 +47,30 @@
 // 
 // 
 // ## INSTALLATION
+// 
 // compile and burn the ino file to your ESP / Wemos board.Connect the four lines to your display / tablet computer.
 // 
 // 
 // ## AUTHOR
+// 
 // Oliver Abraham, mail@oliver - abraham.de, https://www.oliver-abraham.de
 // Please feel free to commentand suggest improvements!
 // 
 // 
 // ## SOURCE CODE
-// The source code is hosted at :
-// https://github.com/OliverAbraham/DashboardPowerManager
+// 
+// The source code is hosted at https://github.com/OliverAbraham/DashboardPowerManager
+//
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 int _motionSensorLastValue = 0;
 int _powerUpWaitTimeBeforeDoingAnything = 10;
+int _motionSensorEventsBeforeSwitchingOnTheScreen = 2; // to prevent display wakeup at the first motion event
+int _inertia = 0;
+int _inertiaResetCounter = 0;
+int _secondsToWaitBeforeClickingAgain = 120; // 2 minutes between mouse clicks, in case you're trying to operate the monitor. So the mouse clicks won't disturb you too much.
 
 void setup()
 {
@@ -84,23 +102,30 @@ void loop()
     int powerIsOn = CheckIfComputerIsOn();
     int motionSensor = CheckTheMotionSensor();
 
-    printf("motion sensor: %s    dashboard power is: %s\n", motionSensor ? "ON" : "off", powerIsOn ? "ON" : "off");
-
     if (_powerUpWaitTimeBeforeDoingAnything > 0)
     {
         _powerUpWaitTimeBeforeDoingAnything--;
+        printf("power up wait time %d\n", _powerUpWaitTimeBeforeDoingAnything);
     }
     else
     {
+        printf("motion sensor: %s    dashboard power: %s  inertia: %d  inertia reset: %d\n", motionSensor ? "ON " : "off", powerIsOn ? "ON " : "off", _inertia, _inertiaResetCounter);
         if (MotionSensorGoesOn())
         {
             if (powerIsOn)
-                ReEnableScreen();
+            {
+                if (WaitTimeIsOver())
+                {
+                    ReEnableScreen();
+                    WaitBeforeClickingAgain();
+                }
+            }
             else
                 SwitchOnComputer();
         }
     }
 
+    ResetCounter();
     FlashInternalLed();
     delay(1000);
 }
@@ -137,13 +162,53 @@ int CheckIfComputerIsOn()
 //    return powerIsOn;
 //}
 
+int WaitTimeIsOver()
+{
+    _inertia++;
+    if (_inertia >= _motionSensorEventsBeforeSwitchingOnTheScreen)
+    {
+        printf("motion sensor: %s    dashboard power: %s  inertia: %d  inertia reset: %d\n", true ? "ON " : "off", true ? "ON " : "off", _inertia, _inertiaResetCounter);
+        return 1;
+    }
+    else
+    {
+        _inertiaResetCounter = 60;
+        return 0;
+    }
+}
+
+void ResetCounter()
+{
+    if (_inertiaResetCounter > 0)
+    {
+        _inertiaResetCounter--;
+        if (_inertiaResetCounter == 0)
+        {
+            _inertia = 0;
+        }
+    }
+}
+
 void ReEnableScreen()
 {
     // When the motion sensor goes on, press the left mouse button short two times 
     // the second time to remove the lock screen on some computers
     PressLeftMouseButton();
+    
+    printf("Waiting 3 seconds\n"); 
     delay(3000);
+
     PressLeftMouseButton();
+}
+
+void WaitBeforeClickingAgain()
+{
+    for (int i = _secondsToWaitBeforeClickingAgain; i > 0; i--)
+    {
+        printf("Waiting before doing anything %d\n", i);
+        delay(1000);
+    }
+    _inertia = 0;
 }
 
 void SwitchOnComputer()
@@ -163,6 +228,7 @@ void SwitchOnComputer()
         digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off (HIGH is the voltage level)
         delay(2000);
     }
+    _inertia = 0;
     printf("\n");
 }
         
@@ -178,6 +244,10 @@ void PressLeftMouseButton()
 
 void PowerUpGreeting()
 {
+    printf("\n\n\n\n\n\n\n--------------------------------------------------------------\n");
+    printf("Dashboard power manager - mail@oliver-abraham.de\n");
+    printf("--------------------------------------------------------------\n\n\n\n");
+
     for (int i = 0; i < 20; i++)
     {
         FlashInternalLed();
